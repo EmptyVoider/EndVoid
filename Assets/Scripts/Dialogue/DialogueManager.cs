eaa03d23b9456ffbf18557bea7f3b9906143068e
jaelkwakkel <jaelkwakkel@gmail.com>
2020-10-08 15:32:09 +0200

    Added sounds for dialogue boxes
    Customizable sounds for every sentence said.

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+    private Queue<AudioClip> sentenceSounds;
+
+    public AudioSource sourceAudio;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+        sentenceSounds = new Queue<AudioClip>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.npcName;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        sentenceSounds.Clear();
+
+        foreach (AudioClip voiceClip in dialogue.sentenceSound)
+        {
+            sentenceSounds.Enqueue(voiceClip);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        sourceAudio.PlayOneShot(sentenceSounds.Dequeue());
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("Finished conversation");
+    }
+
+}
