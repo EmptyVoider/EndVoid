ff318e80111c1b7708384210019b69802552956e
Tomer-PC\Tomer <tomerssh@gmail.com>
2020-10-23 19:11:00 +0300

    add instruction pop up and envelope trigger

---
+++ Assets/Scripts/Triggers/GenericTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Triggers
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class GenericTrigger : MonoBehaviour
+    {
+        public GameObject[] ObjectsToTrigger;
+        public bool SingleUse;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            foreach (var triggerableObject in ObjectsToTrigger)
+            {
+                if (!triggerableObject.gameObject.activeInHierarchy)
+                {
+                    triggerableObject.gameObject.SetActive(true);
+                }
+            }
+
+            if (SingleUse)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
